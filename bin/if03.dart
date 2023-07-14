30f8a23e227561b43656717f3aa687a328efd976
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-14 09:30:56 +0500

    if-03

--- bin/if03.dart
+++ bin/if03.dart
@@ -7,5 +7,46 @@ Args:
     c: Third number.
 Returns:
     int: return answer. */
-void main() {
+
+
+func(int a, int b, int c){
+  if(a>b){
+    if(a<c){
+      return a;
+    }
+  } else {
+    if(a>c){
+      return a;
+    }
+  }
+
+  if(b>a){
+    if(b<c){
+      return b;
+    }
+  } else {
+    if(b>c){
+      return b;
+    }
+  }
+
+  if(c>a){
+    if(c<b){
+      return c;
+    }
+  } else {
+    if(c>b){
+      return c;
+    }
+  }
 }
+void main(){
+  int a = 7;
+  int b = 3;
+  int c = 5;
+  int result = func(a,b,c);
+  print(result);
+}
+
+
+
