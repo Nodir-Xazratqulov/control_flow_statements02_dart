552e6a349c177ce6fcf55717db12903065a3a930
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-14 09:30:05 +0500

    if-01

--- bin/if01.dart
+++ bin/if01.dart
@@ -8,5 +8,23 @@ Args:
 Returns:
     int: return answer.
 */
-void main() {
+
+
+int func(int a, int b, int c){
+  if(a>b){
+    if(a>c){
+      return a;
+    } else {
+      return c;
+    }
+  } else {
+    if(b>c){
+      return b;
+    } else {
+      return c;
+    }
+  }
+}
+void main(){
+  print(func(1,2,3));
 }
