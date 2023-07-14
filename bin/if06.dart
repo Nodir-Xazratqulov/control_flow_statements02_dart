8c8cab6adabfea882220948c7d9fb2c16d0e276d
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-14 09:32:10 +0500

    if-06

--- bin/if06.dart
+++ bin/if06.dart
@@ -6,5 +6,48 @@ Args:
 Returns:
     int: return answer. 
 */
+
+
+func(int n){
+  int a = n%10; // 3
+  int b = n~/10;
+  int c = b%10; // 6
+  int d = b~/10;
+  int e = d%10; // 5
+  int f = d~/10; // 8
+  if(a>c){
+    if (a>e) {
+      if (a>f) {
+        return 3;
+      }
+    }
+  }
+  
+  if(c>a){
+    if (c>e) {
+      if (c>f) {
+        return 2;
+      }
+    }
+  }
+
+  if(e>a){
+    if (e>c) {
+      if (e>f) {
+        return 1;
+      }
+    }
+  }
+
+  if(f>a){
+    if (f>e) {
+      if (f>c) {
+        return 0;
+      }
+    }
+  }
+}
+
 void main() {
+  print(func(3671));
 }
