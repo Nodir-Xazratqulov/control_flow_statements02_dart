b7e4efac3f87307d2356bf6a575a8c3981477f9d
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-07-14 09:31:38 +0500

    if-05

--- bin/if05.dart
+++ bin/if05.dart
@@ -7,5 +7,47 @@ Returns:
     int: return answer.
 */
 
-void main() {
+func(int n){
+  int a = n%10; // 3
+  int b = n~/10;
+  int c = b%10; // 6
+  int d = b~/10;
+  int e = d%10; // 5
+  int f = d~/10; // 6
+  if(a>c){
+    if(a>e){
+      if(a>f){
+        return a;
+      }
+    }
+  }else if(c>a){
+    if(c>e){
+      if(c>f){
+        return c;
+      }
+    }
+  }else if(e>a){
+    if(e>c){
+      if(e>f){
+        return e;
+      }
+    }
+  }else if(f>a){
+    if (f>c) {
+      if (f>e) {
+        return f;
+      }
+    }
+  }
 }
+
+
+
+
+void main() {
+  print(func(2309));
+}
+
+
+
+
